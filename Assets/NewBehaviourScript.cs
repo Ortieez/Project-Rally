f5a476dd36f7d07a7eb1455237b5318739fbf28f
Adam Lipert <a.lipert@protonmail.com>
2023-10-13 16:41:11 +0200

    first commit hihi

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // write simple camera movement script
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*var camera = GetComponent<Camera>();
+        var cameraTransform = camera.transform;
+        var cameraPosition = cameraTransform.position;
+        var cameraForward = cameraTransform.forward;
+        var cameraRight = cameraTransform.right;
+            
+        var cameraSpeed = 10f;
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            cameraPosition += cameraForward * cameraSpeed * Time.deltaTime;
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            cameraPosition -= cameraForward * cameraSpeed * Time.deltaTime;
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            cameraPosition -= cameraRight * cameraSpeed * Time.deltaTime;
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            cameraPosition += cameraRight * cameraSpeed * Time.deltaTime;
+        }
+        
+        cameraTransform.position = cameraPosition;*/
+    }
+}
