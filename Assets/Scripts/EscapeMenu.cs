43803f6fe48a3aa34fc2e144597057dd00a11a8a
Adam Lipert <a.lipert@protonmail.com>
2023-11-12 23:22:11 +0100

    update

---
+++ Assets/Scripts/EscapeMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject.Find("CanvasEscaped").GetComponent<Canvas>().enabled = false;
+        Time.timeScale = 1;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale != 0)
+            {
+                Time.timeScale = 0;
+                GameObject.Find("CanvasEscaped").GetComponent<Canvas>().enabled = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                GameObject.Find("CanvasEscaped").GetComponent<Canvas>().enabled = false;
+            }
+        }
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+}
